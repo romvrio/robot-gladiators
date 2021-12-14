6e5a5084e22b7b00320a3124c917ea618ee30066
Romario <morrisromo9966@gmail.com>
2021-12-14 17:00:05 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,8 +2,7 @@
 // WIN - Player robot has defeated all enemy-robots
 // * Fight all enemy-robots
 // * Defeat each enemy-robot
-// Lose - Players robot's Health is zero or less
-
+// Lose - Players robot's Health is zero or les
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -18,7 +17,7 @@ var enemyAttack = 12;
 var fight = function (enemyName) {
 
     // repeat and execute as as the enemy-robot is alive
-    while (playerHealth > 0 && enemyHealth > 0) {
+    while (playerHealth >= 0 && enemyHealth >= 0) {
         var promptFight = window.prompt("Would you like to Fight or SKIP this battle? Enter 'FIGHT' or ' SKIP' to choose.");
 
         // if player choses to skip
@@ -28,13 +27,16 @@ var fight = function (enemyName) {
             // if yes (true) leave the fight
             if (confirmSkip) {
                 window.alert(playerName + " has chosen to skip this fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
+            else window.alert("Invalid entry, try again.");
         }
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -52,7 +54,8 @@ var fight = function (enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
         // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -76,41 +79,44 @@ var startGame = function () {
     playerMoney = 10;
 
     for (var i = 0; i < enemyNames.length; i++) {
-        if (playerHealth > 0) {
+        if (playerHealth >= 0) {
             //let player know what round they are in, arrays atart t 0 so it needs 1 added to it
             window.alert("welcome to Robot Gladiators! Round " + (i + 1));
             // pick new enemy to fight based on the undex of enemyNames array
             var pickedEnemyName = enemyNames[i];
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             // call fight function with enemy-robot
             fight(pickedEnemyName);
+
             // if were not at the last enemy in the array
-            if (playerHealth > 0 && i < enemyNames.length - 1) {
-                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?")
-                shop();
+            if (playerHealth >= 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
 
                 //if yes, take them to store() function
                 if (storeConfirm) {
                     shop();
                 }
+                else {
+                    window.alert("You have lost your robot in battle! Game Over!");
+                    break;
+                };
             }
         }
-        else {
-            window.alert("You have lost your robot in battle! Game Over!");
-            break;
-        };
-        endGame();
-
     }
-}
+    endGame();
+};
 
 var endGame = function () {
     // if player is still alive, player wins!
 
-    if (playerHealth > 0) {
+    if (playerHealth >= 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("The game has now ended. Let's see how you did!");
     }
+
+
 
     // ask player if theyd like to play again 
     var playAgainConfirm = window.confirm("Would you like to play again?");
@@ -119,16 +125,17 @@ var endGame = function () {
         //restart the game
         startGame();
     }
-
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-
+    shop();
 };
 
-var shop = function () {
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
 
+var shop = function () {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
     // use switch to carry out action
     switch (shopOptionPrompt) {
         case "REFILL":
@@ -142,6 +149,7 @@ var shop = function () {
             }
             else {
                 window.alert("You don't have enough money!");
+                shop();
             }
             break;
         case "UPGRADE":
@@ -155,6 +163,7 @@ var shop = function () {
             }
             else {
                 window.alert("You don't have enough money!");
+                shop();
             }
             break;
         case "LEAVE":
@@ -169,14 +178,24 @@ var shop = function () {
             // call shop() again to force player to pick a valid option
             shop();
             break;
+
     }
+};
 
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
 
-
+    return value;
 };
 startGame();
 
+/* to do list:
+fight function will continue function even if player puts in wrong entry. 
 
+"Robot has died repeats twice in endgame" 
+
+round numbers will remain at 1 and not increase by 'i' 
+*/
 
 
 
